485ac419ebeffca488aaa69e745d20d60b4c780e
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-02 00:14:27 +0200

    create custom hook

---
+++ src/hooks/useStorage.js
@@ -0,0 +1,16 @@
+import {useState, useEffect} from 'react'
+import{projectStorage} from "../firebase/config";
+
+const useStorage = (file) => {
+    const [progress, setProgress] = useState(0)
+    const [error, setError] = useState(null)
+    const [url, setUrl] = useState(null)
+
+
+    useEffect(()=> {
+        const storageRef = projectStorage.ref(file.name);
+        storageRef.put(file).on('state_changed', (snap)=> {
+
+        })
+    }, [file])
+}
