85b0b7c45a9b37cd3af57ee896e9148113d7be4d
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-01 12:01:39 +0200

    add firebase to project

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import * as firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDMfheufuE-xNsbajL44BP8o1PhUfunzig",
+    authDomain: "firegram-cad.firebaseapp.com",
+    databaseURL: "https://firegram-cad.firebaseio.com",
+    projectId: "firegram-cad",
+    storageBucket: "firegram-cad.appspot.com",
+    messagingSenderId: "146025023828",
+    appId: "1:146025023828:web:63c02b9d7685bf963120c1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
