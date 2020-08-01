386a93c1d3633a918cc4f3f54a845b3fbb1d8953
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-01 23:56:00 +0200

    add useState

---
+++ src/components/UploadForm.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+
+const UploadForm = () => {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const changeHandler = (e) => {
+        let selectedFile = e.target.files[0]
+
+        const allowedTypes = ['image/png','image/jpeg']
+
+        if(selectedFile && allowedTypes.includes(selectedFile.type)){
+            setFile(selectedFile)
+            setError('')
+        } else{
+            setFile(null)
+            setError('Select image in png or jpeg format')
+        }
+    }
+
+    return (
+        <form>
+            <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                {error && <div className="error">{error} </div> }
+                {file && <div>{file.name} </div> }
+            </div>
+        </form>
+    )
+};
+
+export default UploadForm
