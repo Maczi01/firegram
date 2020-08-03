90dd574a2fad2786e2c2d9fc4511ebef9b6a1e32
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-03 23:52:28 +0200

    create timestampp

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -16,8 +16,9 @@ firebase.initializeApp(firebaseConfig);
 
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
-
+const timestamp = firebase.firestore.FieldValue.serverTimestamp()
 export {
     projectStorage,
-    projectFirestore
+    projectFirestore,
+    timestamp
 }
