9ddf8c2262cf311333275604c15254344fc9edcd
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-04 22:21:46 +0200

    add second custom hook - first step

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -1,2 +1,20 @@
 import {useState, useEffect} from 'react'
 import {projectStorage, projectFirestore, timestamp} from "../firebase/config";
+
+const useFirestore = (collection) => {
+    const [docs, setDocs] = useState([]);
+
+    useEffect(() => {
+        projectFirestore.collection(collection)
+            .orderBy('createdAt', 'desc')
+            .onSnapshot((snap) => {
+            let documents = []
+            snap.forEach(doc => {
+                documents.push({...doc.data(), id: doc.id})
+            })
+            setDocs(documents)
+        })
+    }, [collection])
+
+    return {docs}
+}
