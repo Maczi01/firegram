4665b3b40dbdcee8b978aa4f399f8d6e5552cf2a
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-14 00:02:04 +0200

    add items to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,10 @@ const Header = () => (
     <div className={styles.header}>
         <img src={logoImage} className={styles.logo} alt="logo"/>
         <ul className={styles.list}>
-            <li className={styles.item}>me</li>
-            <li>you</li>
+            <li className={styles.item}>My projects</li>
+            <li className={styles.item}>My cad models</li>
+            <li className={styles.item}>About me</li>
+            <li className={styles.item}>My CV</li>
         </ul>
     </div>
 )
