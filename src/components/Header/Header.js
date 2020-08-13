03f92a1c91c4f8ede0bade117da08d70029173a6
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-13 23:45:36 +0200

    add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from './Header.module.scss';
+import logoImage from '../../assets/img/logo.png';
 
 const Header = () => (
-        <ul className={styles.header}>
-            <li>me</li>
+    <div className={styles.header}>
+        <img src={logoImage} className={styles.logo} alt="logo"/>
+        <ul className={styles.list}>
+            <li className={styles.item}>me</li>
             <li>you</li>
         </ul>
+    </div>
 )
 
 export default Header
