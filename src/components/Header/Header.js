3abdb04f0947557e4c668e1f229f33fd8de286d9
Maczi01 <mateusz.w.twardy@gmail.com>
2020-08-13 23:23:33 +0200

    add scss (css) to header. First working version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from './Header.module.scss';
 
 const Header = () => (
-        <ul className="footer">
+        <ul className={styles.header}>
             <li>me</li>
             <li>you</li>
         </ul>
